a86aec73945369f868204e2b4c71f136f5cf46ed
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-08-17 20:20:02 +0100

    import all needed files and add gamescene

---
+++ src/Scenes/GameScene.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-mixed-operators */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-plusplus */
+import { Scene } from 'phaser';
+import { storeScore } from '../localStorage';
+import ScoreLabel from '../ScoreLabel';
+import {
+  Player, ChaserShip, GunShip, CarrierShip, ScrollingBackground,
+} from '../Entities';
+
+export default class GameScene extends Scene {
+    constructor() {
+        super('Game');
+        this.score = 0;
+        this.scoreLabel = undefined;
+        this.gameOptions = {
+          initialHealth: 100,
+        };
+      }
+}
