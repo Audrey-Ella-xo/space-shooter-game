ddc4ea2bc86a1910c3c5fa3ac99e0c403bd067b2
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-08-17 19:46:13 +0100

    Add api setup and local storage setup

---
+++ src/api.js
@@ -0,0 +1,70 @@
+import 'regenerator-runtime';
+
+const fetch = require('node-fetch');
+
+const createGame = async () => {
+  const game = {
+    name: "Ella's Platform Game",
+  };
+  const post = JSON.stringify(game);
+  const address = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  const settings = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: post,
+  };
+  const response = await fetch(address, settings);
+  const result = await response.json();
+  return result;
+};
+const ID = 'Yl2GhSyLcSxu1uOnNdaE';
+const submitScore = async (name, score) => {
+  const submit = {
+    user: name,
+    score,
+  };
+  const post = JSON.stringify(submit);
+  const address = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${ID}/scores/`;
+  const settings = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: post,
+  };
+  const response = await fetch(address, settings);
+  const result = await response.json();
+  return result;
+};
+
+const sortScore = (obj) => {
+  const array = [];
+  for (let i = 0; i < obj.length; i += 1) {
+    array.push([obj[i].score, obj[i].user]);
+  }
+  return Array.from(array).sort((a, b) => b[0] - a[0]);
+};
+
+const getScoreBoard = async () => {
+  const address = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${ID}/scores/`;
+  const settings = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = await fetch(address, settings);
+  const answer = await response.json();
+  return sortScore(answer.result);
+};
+
+export {
+  submitScore,
+  createGame,
+  getScoreBoard,
+};
